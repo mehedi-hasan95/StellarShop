fe2ac0d7ead8e7bcf94d221395ee47b4850ef5d8
mehedi-hasan95 <mh.me.mehedi@gmail.com>
2023-11-23 17:39:02 +0600

    Category done

---
+++ src/app/api/category/route.ts
@@ -0,0 +1,24 @@
+import prismadb from "@/lib/prismadb";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+    const { name, image } = body;
+    const category = await prismadb.category.create({
+      data: { name, image },
+    });
+    return NextResponse.json({ msg: "success", category });
+  } catch (error) {
+    return NextResponse.json({ msg: "faill", error });
+  }
+}
+
+export async function GET(req: Request) {
+  try {
+    const category = await prismadb.category.findMany();
+    return NextResponse.json({ msg: "success", category });
+  } catch (error) {
+    return NextResponse.json({ msg: "faill", error });
+  }
+}
