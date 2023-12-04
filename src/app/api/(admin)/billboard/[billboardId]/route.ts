ced8a72aaa22ce74e6316346a62efaca86b6e1de
mehedi-hasan95 <mh.me.mehedi@gmail.com>
2023-12-04 17:55:40 +0600

    Add bilboard patch api

--- src/app/api/(admin)/billboard/[billboardId]/route.ts
+++ src/app/api/(admin)/billboard/[billboardId]/route.ts
@@ -1,9 +1,9 @@
 import { getAuthSession } from "@/app/api/auth/[...nextauth]/route";
 import prismadb from "@/lib/prismadb";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function PATCH(
-  req: Request,
+  req: NextRequest,
   { params }: { params: { billboardId: string } }
 ) {
   try {
@@ -11,15 +11,14 @@ export async function PATCH(
     if (session?.user.role !== "admin") {
       return new NextResponse("Unauthorize user", { status: 401 });
     }
-    const body = req.json();
+    const body = await req.json();
     const { label, image, catId } = body as any;
-    console.log(params.billboardId);
-    // if (!label || !image || !catId) {
-    //   return NextResponse.json(
-    //     { error: "Missing required properties" },
-    //     { status: 400 }
-    //   );
-    // }
+    if (!label || !image || !catId) {
+      return NextResponse.json(
+        { error: "Missing required properties" },
+        { status: 400 }
+      );
+    }
     const billboard = await prismadb.billboard.update({
       where: {
         id: params.billboardId,
@@ -28,6 +27,22 @@ export async function PATCH(
         label,
         image,
         catId,
+      },
+    });
+    return NextResponse.json({ msg: "success", billboard });
+  } catch (error) {
+    return NextResponse.json({ msg: "faill", error });
+  }
+}
+
+export async function GET(
+  req: Request,
+  { params }: { params: { billboardId: string } }
+) {
+  try {
+    const billboard = await prismadb.billboard.findUnique({
+      where: {
+        id: params.billboardId,
       },
     });
     return NextResponse.json({ msg: "success", billboard });
