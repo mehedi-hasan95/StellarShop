ed10836fa103c4ed732203de5faf859599dedbe0
mehedi-hasan95 <mh.me.mehedi@gmail.com>
2023-12-18 17:17:19 +0600

    Table view add to User and seller component

--- src/app/admin/(adminPages)/user/page.tsx
+++ src/app/admin/(adminPages)/user/page.tsx
@@ -1,32 +1,66 @@
 "use client";
-
+import { format } from "date-fns";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 import Image from "next/image";
 import useSWR from "swr";
+import { Separator } from "@/components/ui/separator";
+import { cn } from "@/lib/utils";
+
+interface UserProps {
+  id: string;
+  image: string;
+  name: string;
+  email: string;
+  createdAt: string;
+}
 
 const User = () => {
   const { data } = useSWR("/api/admin/user");
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-      {data?.user?.map((item: any, i: number) => (
-        <div key={item.id}>
-          <p className="flex gap-2 items-center">
-            {i + 1}.{" "}
-            <Image
-              src={
-                item.image !== undefined && item.image !== null
-                  ? item.image
-                  : "/logo.svg"
+    <Table>
+      <TableCaption>
+        <Separator className={cn("bg-emerald-500")} />
+        <p className="pt-5 text-md font-bold text-black">A list of User.</p>
+      </TableCaption>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="">Profile Picture</TableHead>
+          <TableHead>Name</TableHead>
+          <TableHead>Email</TableHead>
+          <TableHead>Open Account</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {data?.user?.map((item: UserProps) => (
+          <TableRow key={item.id}>
+            <TableCell>
+              {
+                <Image
+                  src={item.image}
+                  alt=""
+                  height={500}
+                  width={500}
+                  className="h-10 w-10 rounded-full"
+                />
               }
-              alt=""
-              width={500}
-              height={500}
-              className="h-7 w-7 rounded-full"
-            />{" "}
-            {item.name}
-          </p>
-        </div>
-      ))}
-    </div>
+            </TableCell>
+            <TableCell>{item.name}</TableCell>
+            <TableCell>{item.email}</TableCell>
+            <TableCell>
+              {format(new Date(item.createdAt), "dd-MMM-yyyy")}
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
   );
 };
 
