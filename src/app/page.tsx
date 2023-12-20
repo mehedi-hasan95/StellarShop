cc6befd3eb35976bce7edc731dc4c54a85250d99
mehedi-hasan95 <mh.me.mehedi@gmail.com>
2023-12-20 23:47:07 +0600

    Save some change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,60 +6,20 @@ import Trending from "@/components/custom/home/Trending";
 import PopulerPosts from "@/components/custom/home/PopulerPosts";
 import LatestPosts from "@/components/custom/home/LatestPosts";
 import Footer from "@/components/custom/Footer";
+import { Suspense } from "react";
 import SkeletonLoder from "@/components/custom/SkeletonLoder";
-import Image from "next/image";
-import Link from "next/link";
 
-async function getCategoryData() {
-  try {
-    const res = await fetch(process.env.BASE_URL + `/admin/category`, {
-      cache: "no-store",
-    });
-
-    if (!res.ok) {
-      throw new Error("Failed to fetch data");
-    }
-
-    return res.json();
-  } catch (error) {
-    console.error(error);
-  }
-}
-export default async function Home() {
-  const data = await getCategoryData();
-  console.log(data);
+export default function Home() {
   return (
     <div>
       <MainMenu />
       <SearchBar />
       <div className="container mx-auto p-4">
         <h2 className="text-lg font-bold py-10">Search by Category:</h2>
-        {/* Category data  */}
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-4 gap-y-10">
-          {data?.category?.map((item: any) => (
-            <Link
-              href={`/category/${item.slug}`}
-              key={item.id}
-              className="flex gap-5 items-center"
-            >
-              <Image
-                src={item.image}
-                alt=""
-                height={500}
-                width={500}
-                className="h-20 w-20"
-              />
-              <div>
-                <h4 className="text-lg">{item.name}</h4>
-                <p>{item._count.products} Items</p>
-              </div>
-            </Link>
-          ))}
-        </div>
-        {/* Category data  */}
+        <ByCategory />
       </div>
       <CreateAccount />
-      {/* <div className="container mx-auto p-4">
+      <div className="container mx-auto p-4">
         <h2 className="text-xl md:text-2xl font-bold py-2 border-b-2 border-emerald-500 max-w-max mb-5">
           Our Populer Products
         </h2>
@@ -76,7 +36,7 @@ export default async function Home() {
           Our Latest product:
         </h2>
         <LatestPosts />
-      </div> */}
+      </div>
       <Footer />
     </div>
   );
--- src/components/custom/home/ByCategory.tsx
+++ src/components/custom/home/ByCategory.tsx
@@ -1,8 +1,21 @@
 import Image from "next/image";
 import Link from "next/link";
 
-const ByCategory = ({ data }: any) => {
-  console.log(data);
+async function getCategoryData() {
+  try {
+    const res = await fetch(process.env.BASE_URL + `/admin/category`);
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch data");
+    }
+
+    return res.json();
+  } catch (error) {
+    console.error(error);
+  }
+}
+const ByCategory = async () => {
+  const data = await getCategoryData();
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-4 gap-y-10">
       {data?.category?.map((item: any) => (
