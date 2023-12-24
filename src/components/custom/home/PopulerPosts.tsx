a1a469b178ff8b61a75a37f528f5ad5213d83cbe
mehedi-hasan95 <mh.me.mehedi@gmail.com>
2023-12-24 18:15:29 +0600

    Remove slider unused data

--- src/components/custom/home/PopulerPosts.tsx
+++ src/components/custom/home/PopulerPosts.tsx
@@ -15,29 +15,7 @@ async function getPopulerData() {
 }
 const PopulerPosts = async () => {
   const data = await getPopulerData();
-  return (
-    <Slider
-      data={data}
-      autoPlay={{ delay: 2500, disableOnInteraction: false }}
-      slidesPerView={1}
-      spaceBetween={30}
-      pagination={{ clickable: true, dynamicBullets: true }}
-      breakpoints={{
-        640: {
-          slidesPerView: 1,
-          spaceBetween: 20,
-        },
-        768: {
-          slidesPerView: 2,
-          spaceBetween: 20,
-        },
-        1024: {
-          slidesPerView: 3,
-          spaceBetween: 30,
-        },
-      }}
-    />
-  );
+  return <Slider data={data} className="lg:basis-1/4" />;
 };
 
 export default PopulerPosts;
